2c570ce712b34fcefb7807688a8b8bd91c4cd180
hamed aljamry <hamed970@hotmail.com>
2022-11-02 10:17:11 +0000

    testing event listener

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hi")
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+
+buttons.map( button => {
+  button.addEventListener('click',(e) =>{
+// console.log('clicked');
+// console.log(e);
+// console.log(e.target);
+// console.log(e.target.innerText);
+// testing if event listener works
+
+  });
+});
